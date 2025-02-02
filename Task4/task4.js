419c9b5936147b117d5f823628ed5053ec80424b
YousefNuman11 <yousefsnuman@gmail.com>
2025-02-02 16:54:30 +0300

    update files

---
+++ Task4/task4.js
@@ -0,0 +1,17 @@
+document.getElementById("sumOfArray").addEventListener("click", function() {
+
+    let array = [];
+    let sum = 0;
+
+
+
+    for(let i = 1; i <= 100; i++){
+
+        array.push(i);
+        sum += i;
+    }
+
+    console.log("The sum is: ", sum);
+
+
+});
