8a8c62aa9ecfe0e2c1b446fec9e796ebbda8b487
YousefNuman11 <yousefsnuman@gmail.com>
2025-02-05 16:50:36 +0300

    update files

--- Task10_11/task10_11.js
+++ Task10_11/task10_11.js
@@ -3,11 +3,16 @@ let button = document.getElementById("ip");
 
 
 function req (){
-    
+    try{
     let fetchReq = fetch("https://api.ipify.org/");
 
-    fetchReq.then(response => response.text())
+    fetchReq
+    .then(response => response.text())
     .then(data => {button.innerHTML = data;});
+    }
+    catch(err){
+        alert("The url is invalid");
+    }
     
 
 }
